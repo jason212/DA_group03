ecc2afb6fc11e1df1a85836c973e1f8a01a1ba22
jason212 <89631112+jason212@users.noreply.github.com>
2021-08-28 15:24:23 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,24 @@
-print('hi this is your grp leader')
-print('test')
-#test1
-print('jiaxiong gay')
+# This is for TAsk 5 , @kenneth can do task 6?
+import requests
+
+r = requests.get('https://brickset.com/sets/year-2000')
+print("\t *", r.status_code)
+
+
+h = requests.head('https://brickset.com/sets/year-2000')
+print("Header:")
+print("****")
+
+for x in h.headers:
+    print("\t ", x, ":", h.headers[x])
+print("****")
+
+
+# This will modify the headers user-agent
+headers = {
+    'User-Agent' : 'Mobile'
+}
+# Test it on an external site
+url2 = 'http://httpbin.org/headers'
+rh = requests.get(url2, headers=headers)
+print(rh.text)
