9d72f2c826e53cb8878f3b03f32ab38b470d5d2c
kennethcmf <87596650+kennethcmf@users.noreply.github.com>
2021-08-29 17:08:06 +0800

    Update and rename Scrapy.test.py to Scrapytest.py

---
+++ Scrapytest.py
@@ -0,0 +1,13 @@
+#TASK 6 -kenneth
+import scrappy
+classNewSpider(scrapytest.Spider):
+    name = "new_spider"
+    start_urls = ["https://brickset.com/sets/year-2000"]
+    def parse(self, response):
+        css_selector = 'img'
+        for x in response.css(css_selector):
+            newsel ='@src'
+            yield{
+                'image Link':x.xpath(newsel).extract_first(),
+            }
+# URL must change right? 
