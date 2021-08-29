ba600b5edc1bfcdd78236c91e788ecf3998af4d2
jason212 <89631112+jason212@users.noreply.github.com>
2021-08-29 16:39:24 +0800

    Update Scrapy.py

--- Scrapy.py
+++ Scrapy.py
@@ -1,1 +1,10 @@
-
+class NewSpider(scrapy.Spider):
+            name = "new_spider"
+             start_urls = ['http://192.168.1.1/index.html']
+            def parse(self, response):
+                      css_selector = 'img'
+                      for x in response.css(css_selector):
+                                    newsel = '@src'
+                                    yield {
+                                           'Image Link': x.xpath(newsel).extract_first(),
+                                    }
